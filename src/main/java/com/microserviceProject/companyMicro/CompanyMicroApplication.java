d05b8aefe2dcf2e51fc88698db939332f0713d63
septopus9 <septopus9@gmail.com>
2024-07-09 19:25:02 +0530

    first commit

---
+++ src/main/java/com/microserviceProject/companyMicro/CompanyMicroApplication.java
@@ -0,0 +1,13 @@
+package com.microserviceProject.companyMicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyMicroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microserviceProject/companyMicro/CompanyMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microserviceProject.companyMicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
